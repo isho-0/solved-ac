ef7d6a0263c82f72778ed974dc5fbf69f7f3fdff
isho-0 <isho010607@gmail.com>
2025-08-31 03:07:46 +0900

    perf: endl 대신 \n 사용하여 불필요한 flush 제거

--- src/problems/problem_1001.cc
+++ src/problems/problem_1001.cc
@@ -8,9 +8,8 @@ void solve_problem_1001() {
     std::cout << "두 정수 A와 B를 입력하세요 (예: 3 2) : ";
     std::cin >> a >> b;
     
-    std::cout << "입력 : A = " << a << ", B = " << b << std::endl;
-    std::cout << "출력 : " << std::endl;
-    std::cout << a - b << std::endl;
+    std::cout << "입력 : A = " << a << ", B = " << b << "\n";
+    std::cout << "출력 : " << a - b << "\n";
     
     std::cout << "================================" << std::endl;
 }
