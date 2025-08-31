913af0ca3b24e976144f14b32ce60a1e4037a4f8
isho-0 <isho010607@gmail.com>
2025-09-01 00:18:20 +0900

    fix: 문제 번호 오기입

--- src/problems/problem_2522.cc
+++ src/problems/problem_2522.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 
 void solve_problem_2522() {
-  std::cout << "=== 2444번 별 찍기-12 문제 해결 ===" << std::endl;
+  std::cout << "=== 2522번 별 찍기-12 문제 해결 ===" << std::endl;
 
   int n;
   std::cout << "N을 입력하세요 : ";
