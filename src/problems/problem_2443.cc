fa1184e940bcd9b1ab203aa87428939068a69a57
isho-0 <isho010607@gmail.com>
2025-08-31 19:17:49 +0900

    feat: 2443번 문제 해결

--- src/problems/problem_2443.cc
+++ src/problems/problem_2443.cc
@@ -2,27 +2,28 @@
 #include <iostream>
 
 void solve_problem_2443() {
-    std::cout << "=== 2443번 별 찍기-6 문제 해결 ===" << std::endl;
-    
-    int n;
-    std::cout << "N을 입력하세요 : ";
-    std::cin >> n;
-    
-    std::cout << "입력 : n = " << n << "\n";
-    std::cout << "출력 : " << "\n";
-    
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n - i; j++) {
-            std::cout << " ";
-        }
-        for (int j = 1; j <= i; j++) {
-            std::cout << "*";
-        }
-        for (int j = 1; j <= i - 1; j++) {
-            std::cout << "*";
-        }
-        std::cout << "\n";
+  std::cout << "=== 2443번 별 찍기-6 문제 해결 ===" << std::endl;
+
+  int n;
+  std::cout << "N을 입력하세요 : ";
+  std::cin >> n;
+
+  std::cout << "입력 : n = " << n << "\n";
+  std::cout << "출력 : " << "\n";
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      if (j <= i - 1) {
+        std::cout << " ";
+      } else {
+        std::cout << "*";
+      }
     }
-    
-    std::cout << "================================" << std::endl;
+    for (int j = n; j > i; j--) {
+      std::cout << "*";
+    }
+    std::cout << "\n";
+  }
+
+  std::cout << "================================" << std::endl;
 }
