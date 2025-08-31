ddd3907786d43ca221fba02aed8efaa62eee5ca8
isho-0 <isho010607@gmail.com>
2025-09-01 00:34:16 +0900

    refactor: 문제 헤더파일 자동 include 로직 및 내용

---
+++ src/problem_includes.h
@@ -0,0 +1,26 @@
+#ifndef PROBLEM_INCLUDES_H
+#define PROBLEM_INCLUDES_H
+
+// 이 파일은 CMake에 의해 자동으로 생성됩니다.
+// problems/ 디렉토리의 모든 문제 헤더 파일들을 자동으로 include합니다.
+
+#include "problems/problem_1000.h"
+#include "problems/problem_1001.h"
+#include "problems/problem_10950.h"
+#include "problems/problem_10952.h"
+#include "problems/problem_2438.h"
+#include "problems/problem_2439.h"
+#include "problems/problem_2440.h"
+#include "problems/problem_2441.h"
+#include "problems/problem_2442.h"
+#include "problems/problem_2443.h"
+#include "problems/problem_2444.h"
+#include "problems/problem_2522.h"
+#include "problems/problem_2523.h"
+#include "problems/problem_2739.h"
+#include "problems/problem_2741.h"
+#include "problems/problem_2742.h"
+#include "problems/problem_8393.h"
+
+
+#endif  // PROBLEM_INCLUDES_H
--- src/problem_manager.cc
+++ src/problem_manager.cc
@@ -1,25 +1,7 @@
 #include "problem_manager.h"
+#include "problem_includes.h"
 #include <iostream>
 #include <string>
-
-// 문제별 헤더 파일들
-#include "problems/problem_1000.h"
-#include "problems/problem_1001.h"
-#include "problems/problem_10950.h"
-#include "problems/problem_10952.h"
-#include "problems/problem_2438.h"
-#include "problems/problem_2439.h"
-#include "problems/problem_2440.h"
-#include "problems/problem_2441.h"
-#include "problems/problem_2442.h"
-#include "problems/problem_2443.h"
-#include "problems/problem_2444.h"
-#include "problems/problem_2522.h"
-#include "problems/problem_2523.h"
-#include "problems/problem_2739.h"
-#include "problems/problem_2741.h"
-#include "problems/problem_2742.h"
-#include "problems/problem_8393.h"
 
 ProblemManager::ProblemManager() {
   // 문제 등록
