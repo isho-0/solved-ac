97330351f4837e3d2f749ee4c52c0177c7dd23a4
isho-0 <isho010607@gmail.com>
2025-09-01 22:43:33 +0900

    feat: 2445번 문제 해결

--- src/problem_includes.h
+++ src/problem_includes.h
@@ -15,6 +15,7 @@
 #include "problems/problem_2442.h"
 #include "problems/problem_2443.h"
 #include "problems/problem_2444.h"
+#include "problems/problem_2445.h"
 #include "problems/problem_2522.h"
 #include "problems/problem_2523.h"
 #include "problems/problem_2739.h"
---
+++ src/problems/problem_2445.h
@@ -0,0 +1,6 @@
+#ifndef PROBLEM_2445_H
+#define PROBLEM_2445_H
+
+void solve_problem_2445();
+
+#endif //PROBLEM_2445_H
