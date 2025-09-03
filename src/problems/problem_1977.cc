75cfec3df4dfe46ac2b04b95c1f055e511089825
isho-0 <isho010607@gmail.com>
2025-09-03 23:58:28 +0900

    fix: 프로젝트 설정 관리 추가

---
+++ src/problems/problem_1977.cc
@@ -0,0 +1,5 @@
+#include "problem_1977.h"
+#include <iostream>
+
+void solve_problem_1977(){
+  }
---
+++ src/problems/problem_1977.h
@@ -0,0 +1,6 @@
+#ifndef PROBLEM_1977_H
+#define PROBLEM_1977_H
+
+void solve_problem_1977();
+
+#endif
