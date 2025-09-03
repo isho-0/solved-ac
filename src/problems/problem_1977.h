b277eb09b85bcd11b38bcbe2ed8d2186f861dd42
isho-0 <isho010607@gmail.com>
2025-09-04 00:18:25 +0900

    feat: 1977번 문제 해결

--- src/problems/problem_1977.h
+++ src/problems/problem_1977.h
@@ -1,6 +1,21 @@
 #ifndef PROBLEM_1977_H
 #define PROBLEM_1977_H
 
+
+class PerfectSquareFinder {
+private:
+  int m;
+  int n;
+  int sum;
+  int min_val;
+
+public:
+  PerfectSquareFinder(int m, int n);
+
+  void find();
+  void print_result() const;
+};
+
 void solve_problem_1977();
 
 #endif
