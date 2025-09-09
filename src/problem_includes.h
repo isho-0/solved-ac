779795820377aedbbfa9942222aafc7a9d8b50fb
isho-0 <isho010607@gmail.com>
2025-09-09 23:55:42 +0900

    fix: 문제 번호 정렬 수정

--- src/problem_includes.h
+++ src/problem_includes.h
@@ -6,9 +6,6 @@
 
 #include "problems/problem_1000.h"
 #include "problems/problem_1001.h"
-#include "problems/problem_10950.h"
-#include "problems/problem_10952.h"
-#include "problems/problem_11098.h"
 #include "problems/problem_1408.h"
 #include "problems/problem_1977.h"
 #include "problems/problem_2438.h"
@@ -30,7 +27,9 @@
 #include "problems/problem_5565.h"
 #include "problems/problem_5635.h"
 #include "problems/problem_8393.h"
-
+#include "problems/problem_10950.h"
+#include "problems/problem_10952.h"
 #include "problems/problem_10984.h"
+#include "problems/problem_11098.h"
 
 #endif  // PROBLEM_INCLUDES_H
