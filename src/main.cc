27446f1029d320aded0c8db8554c55fae531dc44
isho-0 <isho010607@gmail.com>
2025-09-09 23:32:14 +0900

    feat: 빌드 파일 재생성 명령어 추가

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
 #include "problem_manager.h"
 
+// cd /Users/enyo/Documents/GitHub/solved-ac && rm -rf build && mkdir build && cd build && cmake .. && make
 int main() {
     ProblemManager manager;
     manager.run();
