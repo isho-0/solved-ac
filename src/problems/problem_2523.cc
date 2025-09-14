25af7612483899f555e2ccb6dd1a385935c1b7e0
isho-0 <isho010607@gmail.com>
2025-09-15 01:02:07 +0900

    fix: 2523번 문제 정렬 수정

--- src/problems/problem_2523.cc
+++ src/problems/problem_2523.cc
@@ -2,22 +2,22 @@
 #include <iostream>
 
 void solve_problem_2523() {
-  std::cout << "=== 2523번 별 찍기-13 문제 해결 ===" << std::endl;
+    std::cout << "=== 2523번 별 찍기-13 문제 해결 ===" << std::endl;
 
-  int n;
-  std::cout << "N을 입력하세요 : ";
-  std::cin >> n;
+    int n;
+    std::cout << "N을 입력하세요 : ";
+    std::cin >> n;
 
-  std::cout << "입력 : n = " << n << "\n";
-  std::cout << "출력 : " << "\n";
+    std::cout << "입력 : n = " << n << "\n";
+    std::cout << "출력 : " << "\n";
 
-  for (int i = 1; i <= 2 * n - 1; i++) {
-    int stars = (i <= n) ? n - i : i - n;
+    for (int i = 1; i <= 2 * n - 1; i++) {
+        int stars = (i <= n) ? n - i : i - n;
 
-    for (int j = n; j > stars; j--)
-      std::cout << "*";
-    std::cout << "\n";
-  }
+        for (int j = n; j > stars; j--)
+            std::cout << "*";
+        std::cout << "\n";
+    }
 
-  std::cout << "================================" << std::endl;
+    std::cout << "================================" << std::endl;
 }
