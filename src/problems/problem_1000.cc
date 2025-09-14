f215e50ca64160dbc0f3c7984caf03c0be9473be
isho-0 <isho010607@gmail.com>
2025-09-14 22:34:09 +0900

    fix: 1000번 문제 정렬 수정

--- src/problems/problem_1000.cc
+++ src/problems/problem_1000.cc
@@ -3,20 +3,13 @@
 
 void solve_problem_1000() {
     std::cout << "=== 1000번 A+B 문제 해결 ===" << std::endl;
-    
+
     int a, b;
     std::cout << "두 정수 A와 B를 입력하세요 (예: 1, 2) : ";
     std::cin >> a >> b;
-    
+
     std::cout << "입력 : A = " << a << ", B = " << b << "\n";
     std::cout << "출력 : " << a + b << "\n";
-    
+
     std::cout << "================================" << std::endl;
 }
-
-
-
-
-
-
-
--- src/problems/problem_1000.h
+++ src/problems/problem_1000.h
@@ -4,10 +4,3 @@
 void solve_problem_1000();
 
 #endif
-
-
-
-
-
-
-
