fd37ed88e850f5d3d53446a1cfa0d854a046ca98
isho-0 <isho010607@gmail.com>
2025-09-14 21:48:52 +0900

    feat: 2475번 문제 해결

---
+++ src/problems/problem_2475.cc
@@ -0,0 +1,19 @@
+#include "problem_2475.h"
+#include <iostream>
+
+void solve_problem_2475() {
+    std::cout << "=== 2475번 검증수 문제 해결 ===" << std::endl;
+
+    int arr[6];
+    int last_arr_num = 0;
+
+    for(int i = 0; i < 5; i++) {
+        std::cin >> arr[i];
+        last_arr_num += (arr[i] * arr[i]);
+    }
+    arr[5] = last_arr_num % 10;
+
+    std::cout << arr[5] << "\n";
+
+    std::cout << "================================" << std::endl;
+}
---
+++ src/problems/problem_2475.h
@@ -0,0 +1,6 @@
+#ifndef PROBLEM_2475_H
+#define PROBLEM_2475_H
+
+void solve_problem_2475();
+
+#endif
