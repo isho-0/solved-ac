7f6eaf4a1e5d7d5aafb5462961f15bb41235c688
isho-0 <isho010607@gmail.com>
2025-09-14 22:34:54 +0900

    fix: 1001번 문제 정렬 수정

--- src/problems/problem_1001.cc
+++ src/problems/problem_1001.cc
@@ -3,20 +3,13 @@
 
 void solve_problem_1001() {
     std::cout << "=== 1001번 A-B 문제 해결 ===" << std::endl;
-    
+
     int a, b;
     std::cout << "두 정수 A와 B를 입력하세요 (예: 3 2) : ";
     std::cin >> a >> b;
-    
+
     std::cout << "입력 : A = " << a << ", B = " << b << "\n";
     std::cout << "출력 : " << a - b << "\n";
-    
+
     std::cout << "================================" << std::endl;
 }
-
-
-
-
-
-
-
