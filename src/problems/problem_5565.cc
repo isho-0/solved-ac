739f4bf1978fb0be61880fb1cae7ac7c0df8f950
isho-0 <isho010607@gmail.com>
2025-09-15 01:00:48 +0900

    fix: 5565번 문제 정렬 수정

--- src/problems/problem_5565.cc
+++ src/problems/problem_5565.cc
@@ -2,20 +2,20 @@
 #include <iostream>
 
 void solve_problem_5565() {
-  std::cout << "=== 5565번 영수증 문제 해결 ===" << std::endl;
+    std::cout << "=== 5565번 영수증 문제 해결 ===" << std::endl;
 
-  int book_price;
-  int book_max_price;
-  int buy_price_total = 0;
+    int book_price;
+    int book_max_price;
+    int buy_price_total = 0;
 
-  std::cin >> book_max_price;
+    std::cin >> book_max_price;
 
-  for (int i = 0; i < 9; i++) {
-    std::cin >> book_price;
-    buy_price_total += book_price;
-  }
+    for (int i = 0; i < 9; i++) {
+        std::cin >> book_price;
+        buy_price_total += book_price;
+    }
 
-  std::cout << book_max_price - buy_price_total << std::endl;
+    std::cout << book_max_price - buy_price_total << std::endl;
 
-  std::cout << "================================" << std::endl;
+    std::cout << "================================" << std::endl;
 }
