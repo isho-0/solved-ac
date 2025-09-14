4cca4e366bbc3b27ce7174d003d430fceabdabbb
isho-0 <isho010607@gmail.com>
2025-09-14 22:36:42 +0900

    fix: 2438번 문제 정렬 수정

--- src/problems/problem_2438.cc
+++ src/problems/problem_2438.cc
@@ -3,27 +3,20 @@
 
 void solve_problem_2438() {
     std::cout << "=== 2438번 별 찍기-1 문제 해결 ===" << std::endl;
-    
+
     int n;
     std::cout << "N을 입력하세요 : ";
     std::cin >> n;
-    
+
     std::cout << "입력 : n = " << n << "\n";
     std::cout << "출력 : " << "\n";
-    
+
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
             std::cout << "*";
         }
         std::cout << "\n";
     }
-    
+
     std::cout << "================================" << std::endl;
 }
-
-
-
-
-
-
-
