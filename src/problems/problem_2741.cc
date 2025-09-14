25d0ffaa6e8e7751388004918e4f637b5784729b
isho-0 <isho010607@gmail.com>
2025-09-15 01:01:27 +0900

    fix: 2741번 문제 정렬 수정

--- src/problems/problem_2741.cc
+++ src/problems/problem_2741.cc
@@ -3,24 +3,17 @@
 
 void solve_problem_2741() {
     std::cout << "=== 2741번 N 찍기 문제 해결 ===" << std::endl;
-    
+
     int n;
     std::cout << "N을 입력하세요 : ";
     std::cin >> n;
-    
+
     std::cout << "입력 : n = " << n << "\n";
     std::cout << "출력 : " << "\n";
-    
+
     for (int i = 1; i <= n; i++) {
         std::cout << i << "\n";
     }
-    
+
     std::cout << "================================" << std::endl;
 }
-
-
-
-
-
-
-
