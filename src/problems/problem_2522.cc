55a4d2e37c4f4f1fcd67ce1cf455d5531c561b44
isho-0 <isho010607@gmail.com>
2025-09-15 01:02:59 +0900

    fix: 2522번 문제 정렬 수정

--- src/problems/problem_2522.cc
+++ src/problems/problem_2522.cc
@@ -2,25 +2,25 @@
 #include <iostream>
 
 void solve_problem_2522() {
-  std::cout << "=== 2522번 별 찍기-12 문제 해결 ===" << std::endl;
+    std::cout << "=== 2522번 별 찍기-12 문제 해결 ===" << std::endl;
 
-  int n;
-  std::cout << "N을 입력하세요 : ";
-  std::cin >> n;
+    int n;
+    std::cout << "N을 입력하세요 : ";
+    std::cin >> n;
 
-  std::cout << "입력 : n = " << n << "\n";
-  std::cout << "출력 : " << "\n";
+    std::cout << "입력 : n = " << n << "\n";
+    std::cout << "출력 : " << "\n";
 
-  for (int i = 1; i <= 2 * n - 1; i++) {
-    int spaces = (i <= n) ? n - i : i - n;
-    int stars = (i <= n) ? i : 2 * n - i;
+    for (int i = 1; i <= 2 * n - 1; i++) {
+        int spaces = (i <= n) ? n - i : i - n;
+        int stars = (i <= n) ? i : 2 * n - i;
 
-    for (int j = 0; j < spaces; j++)
-      std::cout << " ";
-    for (int j = 0; j < stars; j++)
-      std::cout << "*";
-    std::cout << "\n";
-  }
+        for (int j = 0; j < spaces; j++)
+            std::cout << " ";
+        for (int j = 0; j < stars; j++)
+            std::cout << "*";
+        std::cout << "\n";
+    }
 
-  std::cout << "================================" << std::endl;
+    std::cout << "================================" << std::endl;
 }
