4f7fd8ff8943cea582a9beba0721e082a8a7b800
isho-0 <isho010607@gmail.com>
2025-09-15 01:01:16 +0900

    fix: 2739번 문제 정렬 수정

--- src/problems/problem_2739.cc
+++ src/problems/problem_2739.cc
@@ -3,24 +3,17 @@
 
 void solve_problem_2739() {
     std::cout << "=== 2739번 구구단 문제 해결 ===" << std::endl;
-    
+
     int n;
     std::cout << "N을 입력하세요 : ";
     std::cin >> n;
-    
+
     std::cout << "입력 : n = " << n << "\n";
     std::cout << "출력 : " << "\n";
-    
+
     for (int i = 1; i <= 9; i++) {
         std::cout << n << " * " << i << " = " << n * i << "\n";
     }
-    
+
     std::cout << "================================" << std::endl;
 }
-
-
-
-
-
-
-
