d8daad3507da9d594798778cdc5c41b0e3042775
isho-0 <isho010607@gmail.com>
2025-09-15 01:03:34 +0900

    fix: 10984번 문제 정렬 수정

--- src/problems/problem_10984.cc
+++ src/problems/problem_10984.cc
@@ -2,30 +2,30 @@
 #include <iostream>
 #include <iomanip>
 
-void solve_problem_10984(){
-  std::cout << "=== 10984번 내 학점을 구해줘 문제 해결 ===" << std::endl;
+void solve_problem_10984() {
+    std::cout << "=== 10984번 내 학점을 구해줘 문제 해결 ===" << std::endl;
 
-  int t;
-  std::cin >> t;
+    int t;
+    std::cin >> t;
 
-  while (t--) {
-    int n;
-    std::cin >> n;
+    while (t--) {
+        int n;
+        std::cin >> n;
 
-    int total_c = 0;
-    double weighted_g = 0;
+        int total_c = 0;
+        double weighted_g = 0;
 
-    for (int i = 0; i < n; i++) {
-      int c;
-      double g;
-      std::cin >> c >> g;
+        for (int i = 0; i < n; i++) {
+            int c;
+            double g;
+            std::cin >> c >> g;
 
-      total_c += c;
-      weighted_g += c * g;
+            total_c += c;
+            weighted_g += c * g;
+        }
+
+        std::cout << total_c << " " << std::fixed << std::setprecision(2) << (weighted_g / total_c) << "\n";
     }
 
-    std::cout << total_c << " " << std::fixed << std::setprecision(2) << (weighted_g / total_c) << "\n";
-  }
-
-  std::cout << "================================" << std::endl;
+    std::cout << "================================" << std::endl;
 }
