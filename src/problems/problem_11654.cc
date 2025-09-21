f7b061a761cdd2ae1519ef9d054dd269921ac519
isho-0 <isho010607@gmail.com>
2025-09-21 23:48:36 +0900

    feat: 11654번 문제 해결

---
+++ src/problems/problem_11654.cc
@@ -0,0 +1,13 @@
+#include "problem_11654.h"
+#include <iostream>
+
+void solve_problem_11654() {
+    std::cout << "=== 11654번 아스키 코드 문제 해결 ===" << std::endl;
+
+    char ch;
+    std::cin >> ch;
+
+    std::cout << static_cast<int>(ch);
+
+    std::cout << "================================" << std::endl;
+}
---
+++ src/problems/problem_11654.h
@@ -0,0 +1,6 @@
+#ifndef PROBLEM_11654_H
+#define PROBLEM_11654_H
+
+void solve_problem_11654();
+
+#endif
