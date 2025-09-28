e467df0f125481e9c14a116ddcbe604df9ddd1f2
isho-0 <isho010607@gmail.com>
2025-09-29 02:29:25 +0900

    fix: 10172번 문제 양식 수정

--- src/problems/problem_10172.cc
+++ src/problems/problem_10172.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 
 void solve_problem_10172() {
-    std::cout << "=== 10172 개 문제 해결 ===" << std::endl;
+    std::cout << "=== 10172번 개 문제 해결 ===" << std::endl;
 
     std::cout << "|\\_/|" << "\n";
     std::cout << "|q p|   /}" << "\n";
