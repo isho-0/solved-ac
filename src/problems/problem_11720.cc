cf704d7b87143a1301ce18dff9f3fa86eb144248
isho-0 <isho010607@gmail.com>
2025-09-29 02:30:36 +0900

    fix: 11720번 문제 양식 수정

--- src/problems/problem_11720.cc
+++ src/problems/problem_11720.cc
@@ -3,7 +3,7 @@
 #include <string>
 
 void solve_problem_11720() {
-    std::cout << "=== 11720 숫자의 합 문제 해결 ===" << std::endl;
+    std::cout << "=== 11720번 숫자의 합 문제 해결 ===" << std::endl;
 
     int num;
     std::cin >> num;
