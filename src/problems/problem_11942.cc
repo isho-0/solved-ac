bd9886689d552a4baaa698437baafb9925f5b352
isho-0 <isho010607@gmail.com>
2025-09-29 02:30:42 +0900

    fix: 11942번 문제 양식 수정

--- src/problems/problem_11942.cc
+++ src/problems/problem_11942.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 
 void solve_problem_11942() {
-    std::cout << "=== 11942 고려대는 사랑입니다 문제 해결 ===" << std::endl;
+    std::cout << "=== 11942번 고려대는 사랑입니다 문제 해결 ===" << std::endl;
 
     std::cout << "고려대학교\n";
 
