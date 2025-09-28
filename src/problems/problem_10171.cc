d6a3170b785029189bb2bab22b8e3b6d7019b0a8
isho-0 <isho010607@gmail.com>
2025-09-29 02:29:20 +0900

    fix: 10171번 문제 양식 수정

--- src/problems/problem_10171.cc
+++ src/problems/problem_10171.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 
 void solve_problem_10171() {
-    std::cout << "=== 10171 고양이 문제 해결 ===" << std::endl;
+    std::cout << "=== 10171번 고양이 문제 해결 ===" << std::endl;
 
     std::cout << "\\    /\\" << "\n";
     std::cout << " )  ( ')" << "\n";
